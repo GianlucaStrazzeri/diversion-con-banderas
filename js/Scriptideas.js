f87cc593b519939cdabadeebb939ea2bff74736a
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-11-28 12:33:07 +0100

    Añadiendo transiciones de movimiento al pasar el ratón encima de la lista

--- js/Scriptideas.js
+++ js/Scriptideas.js
@@ -1,35 +1,11 @@
-fetch ("https://restcountries.com/v3/all")
-.then((response)=>{
-    return response.json()
-    .then((data)=>{
-        data.forEach((data)=>console.log(data)) 
-    })
+
+
+
+  //Agregar un eventListener, la estructura podría ser
+li.addEventListener("mouseover",function(li)=>{
+  li.innerHTML = `
+  <div class:"container">
+  <p>Nombre: ${objeto.languages} ${objeto.capital}</p>
+  </div>
+`;
 });
-//                                          Opciones de cambio linea 5                                       //
-//console.log(data) // imprime un array de objetos
-//console.log(data.length) //devuelve 250
-// data.forEach((data)=>console.log(data))  Imprime cada objeto en la consola
-//data.name Imprime cada nombre en la consola
-//data.name.common  ->Imprime el nombre común del país de la bandera en la consola
-//data.forEach((data)=> zonaApariciónBanderas.innerHTML=data[3] // devuelve undefined
-//data.forEach((data)=> zonaApariciónBanderas.innerHTML(data)) // Devuelve Uncaught (in promise) TypeError: zonaApariciónBanderas.innerHTML is not a function
-//forEach((data)=>zonaApariciónBanderas= document.createElement("li"));
-//data.forEach((data)=> zonaApariciónBanderas.innerHTML="bandera1" ) //Imprime bandera 1 (1 vez) No itera por cada objeto
-//.then(zonaApariciónBanderas.innerHTML= response.json() ); //devuelve en la pantalla [object Promise]
-
-
-//Copio estrucutura de setTimeout()para darle un tiempo en responder
-let numeroAleatorio1;
-const promesa1 = new Promise((resolve) => {
-    setTimeout(() => {
-      numeroAleatorio1 = Math.floor(Math.random() * 10);
-      resolve(numeroAleatorio1);
-    }, 1000);
-  });
-
-  promesa1.then((numeroAleatorio1) => {
-    console.log(`El primer numero aleatorio es ${numeroAleatorio1}`);
-    return promesa2;
-  });
-
-  
--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var zonaApariciónBanderas = document.getElementById('countries-list');
 
 
-
-
 //solución gpt
  // Realiza una solicitud Fetch para obtener datos
  fetch('https://restcountries.com/v3/all')
@@ -17,18 +15,41 @@ var zonaApariciónBanderas = document.getElementById('countries-list');
      data.forEach(function(objeto) {
          // Crea un elemento de lista
          var li = document.createElement('li');
+
+         // Crea un contenedor para elementos adicionales que deben aparecer y desaparecer al pasar el ratón encima de los elementos
+         var contenedorAdicional = document.createElement('div');
+         contenedorAdicional.classList.add('elementos-adicionales');
+
          
          li.innerHTML = `
+                        <img src="${objeto.flags[0]}" alt="Bandera de ${objeto.name.common}" heigth="350" width="350">
                         <p>Nombre: ${objeto.name.common}</p>
-                        <p>Población: ${objeto.population}</p>
-                        <p>Región: ${objeto.region}</p>
-                        <img src="${objeto.flags[0]}" alt="Bandera de ${objeto.name.common}" width="100">
                     `;
-         
+
+          // Agrega elementos adicionales al contenedor
+          contenedorAdicional.innerHTML = `
+          <p> Continente: ${objeto.continents}</p>
+          <p>Capital: ${objeto.capital}</p>
+      `;
+
+      // Agrega el contenedor de elementos adicionales al elemento de lista
+      li.appendChild(contenedorAdicional);
+
+      // Agrega el evento al pasar el ratón sobre el elemento
+      li.addEventListener('mouseover', function() {
+        // Muestra el contenedor de elementos adicionales
+        contenedorAdicional.style.display = 'block';
+    });
+
+    // Agrega el evento al salir del ratón del elemento
+    li.addEventListener('mouseout', function() {
+        // Oculta el contenedor de elementos adicionales
+        contenedorAdicional.style.display = 'none';
+    });
 
          // Agrega el elemento de lista al elemento de la lista en HTML
          zonaApariciónBanderas.appendChild(li);
-         
+        // li.addEventListener("click",)
      });
  })
  .catch(error => console.error('Error al obtener datos:', error));
